74fdb69b842b0790798723fdef2eccddd28fa2a0
enigmaSpy <soczekv18@gmail.com>
2026-07-29 17:10:46 +0200

    wip: Update console log to include worldline parsing for improved debugging

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,7 @@
 import electronLogo from './assets/electron.svg'
 
 function App(): React.JSX.Element {
-  console.log(window.api)  
+  console.log(window.api.worldline.parse('C:\\dev\\worldline-test').then(r => console.log(r)))  
   return (
     <>
       <img alt="logo" className="logo" src={electronLogo} />
