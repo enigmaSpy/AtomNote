8412dc235a6e8305d7d7cd6dfaedbbfd80528c54
enigmaSpy <soczekv18@gmail.com>
2026-07-31 22:33:54 +0200

    refactor: Update package dependencies and remove unused ElectronAPI reference

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,8 +1,6 @@
-import { ElectronAPI } from '@electron-toolkit/preload'
 import type {IpcApi} from '../shared/ipc-contract'
 declare global {
   interface Window {
-    electron: ElectronAPI
     api: IpcApi
   }
 }
