fac4e857183bc78886941b25e9d7f7ddb6f3a0c0
enigmaSpy <soczekv18@gmail.com>
2026-08-03 21:56:41 +0200

    refactor: Remove Worldline IPC handler and update TypeScript configuration to include new service files

--- src/main/ipc/worldline.ts
+++
@@ -1,9 +0,0 @@
-import { ipcMain } from "electron";
-import {IPC_CHANNELS} from "../../shared/ipc-contract"
-import { parseWorldline } from "../services/worldline-parser";
-
-export function registerWorldlineIpc():void{
-    ipcMain.handle(IPC_CHANNELS.WORLDLINE_PARSE, async(_event, rootPath: string)=>{
-        return parseWorldline(rootPath);
-    });
-}
