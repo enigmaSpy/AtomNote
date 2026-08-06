cb4be87cf8209013326f1b5f6b4c9d81b92dcff3
enigmaSpy <soczekv18@gmail.com>
2026-08-06 23:30:08 +0200

    feat: Refactor file and json utilities for improved file handling and add updateJson function

--- src/shared/utils/file.ts
+++ src/shared/utils/file.ts
@@ -1,7 +1,20 @@
-import { readFile } from "node:fs/promises";
-import { join } from "node:path";
+import { mkdir, readFile, unlink, writeFile } from "node:fs/promises";
+import { dirname, join } from "node:path";
 
-export function readFileApp(...segments:string[]){
+export function readFileApp(...segments:string[]):Promise<string>{
     const filePath = join(...segments);
     return readFile(filePath, {encoding:'utf-8'});
+}
+
+export async function writeFileApp(context: string,...segments:string[]):Promise<void>{
+    
+        const filePath = join(...segments);
+        await mkdir(dirname(filePath), {recursive:true})
+        await writeFile(filePath, context, {encoding: 'utf-8'});
+}
+
+
+export const deleteFileApp = async (...segments:string[])=>{
+    const filePath = join(...segments);
+    unlink(filePath);
 }
--- src/shared/utils/json.ts
+++ src/shared/utils/json.ts
@@ -86,6 +86,9 @@ export async function writeJson<T>(
             )
         }
         return false
-    }
-    
+    }   
+}
+
+export async function updateJson(){
+
 }
